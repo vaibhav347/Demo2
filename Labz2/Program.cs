81bdbab664cf34ed076fcc9abdedea84423a9d5c
vaibhav347 <vaibhavaher347.com>
2021-09-27 23:00:39 +0530

    ok

---
+++ Labz2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Labz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+     
+        {
+            Class1 obj = new Class1();
+            
+            obj.sub();
+            
+        }
+    }
+}
